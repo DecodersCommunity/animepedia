4cdc066e084afd909881a1a3cb95e78848fd24b4
sakshi2407 <lcs2021020@iiitl.ac.in>
2022-10-17 01:18:20 +0530

    Added animated headings

--- pagination.js
+++ pagination.js
@@ -78,17 +78,4 @@ $(".previous-page").on("click", function(){
     return showPage(currentPage-1);
 })
 
-/* This code is not required for the animation. This is only needed for the repeatation */
 
-
-// Animated text
-$(function(){
-	$('.repeat').click(function(){
-    	var classes =  $(this).parent().attr('class');
-        $(this).parent().attr('class', 'animate');
-        var indicator = $(this);
-        setTimeout(function(){ 
-        	$(indicator).parent().addClass(classes);
-        }, 20);
-    });
-});
--- scrolltotop.js
+++ scrolltotop.js
@@ -3,4 +3,11 @@ document.getElementById("scrolltopBtn").onclick = () => {
     top: 0,
     behavior: "smooth",
   });
+  document.getElementById("animation").classList.remove('animate');
+  document.getElementById("animation").classList.remove('one');
+
+  setTimeout(function(){
+    document.getElementById("animation").classList.add('animate');
+    document.getElementById("animation").classList.add('one');
+  },300);
 };
